6e082860a0f589999a7ce2287b1463ccbc4e4d66
Jaeem Ekram <jekram98@gmail.com>
2021-05-03 00:12:16 -0400

    commit tree 6b7103dbdc76f8fa369f4a4cb594b34b3c9ca7ed

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+
+let num1 = 62;
+let num2 = 1;
+var multiply = num1 * num2;
+
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+
+
+let num3 = 9
+let num4 = 5
+var mod = num3 % num4;
+
+
+
+
+var max = Math.max(1, 2, 3, 20); 
